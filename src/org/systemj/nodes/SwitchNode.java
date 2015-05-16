988d18924c6e538cc23247c726b7ea67b405e3d8
Heejong Park <hpar081@aucklanduni.ac.nz>
2015-05-16 17:41:24 +1200

    Swt

--- src/org/systemj/nodes/SwitchNode.java
+++ src/org/systemj/nodes/SwitchNode.java
@@ -1,5 +1,9 @@
 package org.systemj.nodes;
 
+import java.io.PrintWriter;
+
+import org.systemj.MemoryPointer;
+
 public class SwitchNode extends BaseGRCNode {
 	private String CDName;
 	private String Statename;
@@ -33,6 +37,17 @@ public class SwitchNode extends BaseGRCNode {
 		}
 		
 		return str;
+	}
+
+	@Override
+	public void weirdPrint(PrintWriter pw, MemoryPointer mp, int termcode, int cdi) {
+		pw.println("LDR R0 $"+Integer.toHexString(mp.switchMap.get(Statename)));
+		pw.println("JMP R0; Jump to this switch child");
+		
+		for(int i=0; i<children.size(); i++){
+			pw.print(Statename.toLowerCase()+"@"+i+" ");
+			this.getChild(i).weirdPrint(pw, mp, termcode, cdi);
+		}
 	}
 
 	
