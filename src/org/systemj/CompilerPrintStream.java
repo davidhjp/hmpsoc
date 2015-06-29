1636295e44c230828cd59c4355b71c1a26bed4ad
Michael McKnight <michael@mcknight.ninja>
2015-06-29 14:52:05 +1200

    Removed unwanted TODOs from CompilerPrintStream

--- src/org/systemj/CompilerPrintStream.java
+++ src/org/systemj/CompilerPrintStream.java
@@ -48,97 +48,81 @@ public class CompilerPrintStream extends PrintStream {
 	}
 	@Override
 	public void print(boolean b) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(b);
 	}
 	@Override
 	public void print(char c) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(c);
 	}
 	@Override
 	public void print(int i) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(i);
 	}
 	@Override
 	public void print(long l) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(l);
 	}
 	@Override
 	public void print(float f) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(f);
 	}
 	@Override
 	public void print(double d) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(d);
 	}
 	@Override
 	public void print(char[] s) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(s);
 	}
 	@Override
 	public void print(String s) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(s);
 	}
 	@Override
 	public void print(Object obj) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.print(obj);
 	}
 	@Override
 	public void println() {
-		// TODO Auto-generated method stub
 		if(v)
 			super.println();
 	}
 	@Override
 	public void println(boolean x) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.println(x);
 	}
 	@Override
 	public void println(char x) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.println(x);
 	}
 	@Override
 	public void println(long x) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.println(x);
 	}
 	@Override
 	public void println(float x) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.println(x);
 	}
 	@Override
 	public void println(double x) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.println(x);
 	}
 	@Override
 	public void println(char[] x) {
-		// TODO Auto-generated method stub
 		if(v)
 			super.println(x);
 	}
